tacerus/ldifdiff

commit 4b41c8e81df57f98d365678073a75b81f99a44f3

Write deletions first
A diff might mark an unique object for deletion in order to then
create a new object (for example a case change in a username in a LDAP
database with case insensitive and unique usernames). The deletion
operation then needs to happen first in order for the addition to not
fail with a conflict.

Signed-off-by: Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>

ldifConvertor.go
@@ -34,7 +34,7 @@ func createModifyStr(actionEntry actionEntry) (string, error) {
 	return buffer.String(), nil
 }
 
-func writeLdif(queue <-chan actionEntry, writer *bytes.Buffer, wg *sync.WaitGroup, err *error) {
+func writeLdif(queue <-chan actionEntry, writer *bytes.Buffer, delWriter *bytes.Buffer, wg *sync.WaitGroup, err *error) {
 	defer wg.Done()
 	for actionEntry := range queue {
 		if *err != nil {
@@ -48,9 +48,10 @@ func writeLdif(queue <-chan actionEntry, writer *bytes.Buffer, wg *sync.WaitGrou
 			for _, attr := range attrList {
 				writer.WriteString(attr + "\n")
 			}
+			writer.WriteString("\n")
 		case actionDelete:
-			writer.WriteString(actionEntry.Dn + "\n") //dn
-			writer.WriteString("changetype: delete\n")
+			delWriter.WriteString(actionEntry.Dn + "\n") //dn
+			delWriter.WriteString("changetype: delete\n\n")
 		case actionModify:
 			writer.WriteString(actionEntry.Dn + "\n") //dn
 			writer.WriteString("changetype: modify\n")
@@ -59,11 +60,10 @@ func writeLdif(queue <-chan actionEntry, writer *bytes.Buffer, wg *sync.WaitGrou
 				*err = modifyErr
 				continue
 			}
-			writer.WriteString(modifyStr)
+			writer.WriteString(modifyStr + "\n")
 		default:
 			*err = errors.New("Unexpected LDIF action value: " + string(actionEntry.Action))
 			continue
 		}
-		writer.WriteString("\n") // empty line as record separator
 	}
 }

diff.go
@@ -99,6 +99,7 @@ func arraysEqual(a, b []string) bool {
 
 func compare(source, target *entries, dnList *[]string) (string, error) {
 	var buffer bytes.Buffer
+	var delBuffer bytes.Buffer
 	var err error
 	queue := make(chan actionEntry, 10)
 	var wg sync.WaitGroup
@@ -109,7 +110,7 @@ func compare(source, target *entries, dnList *[]string) (string, error) {
 
 	// Write the file concurrently
 	wg.Add(1) // 1 writer
-	go writeLdif(queue, &buffer, &wg, &err)
+	go writeLdif(queue, &buffer, &delBuffer, &wg, &err)
 
 	// Dn only on source + removal of identical entries
 	skipDnForDelete = make(map[string]bool) // Keep track of dn to skip at Deletion
@@ -132,7 +133,7 @@ func compare(source, target *entries, dnList *[]string) (string, error) {
 	wg.Wait()
 
 	// Return the results
-	return buffer.String(), err
+	return delBuffer.String() + buffer.String(), err
 }
 
 //func elementInArray(a string, array []string) bool {

commit 2bf904d5695ec6eb5485a97d5d6239c3dda4d012

Adapt tests to new deletion ordering
Fixes: 4b41c8e81df5 ("Write deletions first")
Signed-off-by: Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>

ldifConvertor.go
@@ -3,6 +3,7 @@ package ldifdiff
 import (
 	"bytes"
 	"errors"
+	"fmt"
 	"strings"
 	"sync"
 )
@@ -62,7 +63,7 @@ func writeLdif(queue <-chan actionEntry, writer *bytes.Buffer, delWriter *bytes.
 			}
 			writer.WriteString(modifyStr + "\n")
 		default:
-			*err = errors.New("Unexpected LDIF action value: " + string(actionEntry.Action))
+			*err = errors.New(fmt.Sprintf("Unexpected LDIF action value: %d", actionEntry.Action))
 			continue
 		}
 	}

ldifConvertor_test.go
@@ -44,7 +44,7 @@ func TestWriteLdif(t *testing.T) {
 		wg.Done()
 	}(queue)
 
-	go writeLdif(queue, &buffer, &wg, &err)
+	go writeLdif(queue, &buffer, &bytes.Buffer{}, &wg, &err)
 	wg.Wait()
 
 	if err != nil {
@@ -70,7 +70,7 @@ func TestWriteLdifError(t *testing.T) {
 		wg.Done()
 	}(queue)
 
-	go writeLdif(queue, &buffer, &wg, &err)
+	go writeLdif(queue, &buffer, &bytes.Buffer{}, &wg, &err)
 	wg.Wait()
 
 	if err == nil {

t/modify.ldif
@@ -4,9 +4,6 @@ mail: auth2@domain.ext
 phone: +32364564645
 
 dn: some_dn,ou=aAccounts,dc=domain,dc=ext
-changetype: delete
-
-dn: some_dn,ou=aAccounts,dc=domain,dc=ext
 changetype: modify
 add: mail
 mail: auth2@domain.ext

t/result.ldif
@@ -1,12 +1,3 @@
-dn: only_on_source,ou=aAccounts,dc=domain,dc=ext
-changetype: add
-sambaSID: An other samba id something
-
-dn: only_on_source_several_attr,ou=aAccounts,dc=domain,dc=ext
-changetype: add
-mail: auth2@domain.ext
-sambaSID: An other samba id something
-
 dn: dn_on_ou_only_on_target,ou=target,ou=aAccounts,dc=domain,dc=ext
 changetype: delete
 
@@ -18,6 +9,15 @@ changetype: delete
 
 dn: only_on_target_several_attr,ou=aAccounts,dc=domain,dc=ext
 changetype: delete
+
+dn: only_on_source,ou=aAccounts,dc=domain,dc=ext
+changetype: add
+sambaSID: An other samba id something
+
+dn: only_on_source_several_attr,ou=aAccounts,dc=domain,dc=ext
+changetype: add
+mail: auth2@domain.ext
+sambaSID: An other samba id something
 
 dn: mixed,ou=aAccounts,dc=domain,dc=ext
 changetype: modify
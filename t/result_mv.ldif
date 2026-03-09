tacerus/ldifdiff

commit a5dffb69283ae1c64c74ab7f00dc1c78d2bb7e87

Support attributes with strictly ordered values
In some LDAP implementations, ordering of values is relevant.
Accept an optional list of attributes for which ordering is to be
preserved. Attribute names contained in the list will have
modification operations use replace instead of add in case of ordering
differences.
To facilitate this, the internal handling is refactored to work with a
mapping between attribute names and value arrays instead of arrays
containing full LDIF lines.

Signed-off-by: Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>

input.go
@@ -3,7 +3,9 @@ package ldifdiff
 import (
 	"bufio"
 	"errors"
+	"maps"
 	"os"
+	"slices"
 	"sort"
 	"strings"
 	"sync"
@@ -11,12 +13,12 @@ import (
 
 /* Package only functions */
 
-func convertLdifStr(ldifStr string, ignoreAttr []string) (entries, error) {
-	return importRecords(ldifStr, "", ignoreAttr)
+func convertLdifStr(ldifStr string, ignoreAttr []string, strictAttr []string) (entries, error) {
+	return importRecords(ldifStr, "", ignoreAttr, strictAttr)
 }
 
-func importLdifFile(file string, ignoreAttr []string) (entries, error) {
-	entries, err := importRecords("", file, ignoreAttr)
+func importLdifFile(file string, ignoreAttr []string, strictAttr []string) (entries, error) {
+	entries, err := importRecords("", file, ignoreAttr, strictAttr)
 	if err != nil {
 		err = errors.New(err.Error() + " [" + file + "]")
 	}
@@ -61,10 +63,10 @@ func addLineToRecord(line *string, record *[]string, ignoreAttr []string, prevAt
 	return nil
 }
 
-func importRecords(ldifStr, file string, ignoreAttr []string) (entries, error) {
+func importRecords(ldifStr, file string, ignoreAttr []string, strictAttr []string) (entries, error) {
 	var readErr, parseErr error
 	queue := make(chan []string, 10)
-	entries := make(map[string][]string)
+	entries := make(entries)
 
 	// Read and Parse the file concurrently
 	var wg sync.WaitGroup
@@ -75,7 +77,7 @@ func importRecords(ldifStr, file string, ignoreAttr []string) (entries, error) {
 	default: // it's a file
 		go readFile(file, ignoreAttr, queue, &wg, &readErr)
 	}
-	go parse(entries, queue, &wg, &parseErr)
+	go parse(entries, strictAttr, queue, &wg, &parseErr)
 	wg.Wait()
 
 	// Return values
@@ -184,7 +186,7 @@ func readStr(ldifStr string, ignoreAttr []string, queue chan<- []string, wg *syn
 	}
 }
 
-func parse(entries entries, queue <-chan []string, wg *sync.WaitGroup, err *error) {
+func parse(entries entries, strictAttr []string, queue <-chan []string, wg *sync.WaitGroup, err *error) {
 	defer wg.Done()
 	for record := range queue {
 		dn := record[0] // Find dn, should be the first line
@@ -194,8 +196,28 @@ func parse(entries entries, queue <-chan []string, wg *sync.WaitGroup, err *erro
 		}
 
 		// Sort the entries
-		attr := record[1:]
-		sort.Strings(attr)
-		entries[dn] = attr
+		rest := record[1:]
+
+		newEntry := make(entry, len(rest))
+		entries[dn] = make(entry)
+
+		for _, line := range rest {
+			parts := strings.SplitN(line, ": ", 2)
+			attr := parts[0]
+			val := parts[1]
+			newEntry[attr] = append(newEntry[attr], val)
+		}
+
+		for attr := range maps.Keys(newEntry) {
+			if !slices.Contains(strictAttr, attr) {
+				sort.Strings(newEntry[attr])
+			}
+		}
+
+		for _, attr := range slices.Sorted(maps.Keys(newEntry)) {
+			if len(newEntry[attr]) > 0 {
+				entries[dn][attr] = newEntry[attr]
+			}
+		}
 	}
 }

ldifConvertor.go
@@ -4,7 +4,8 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
-	"strings"
+	"maps"
+	"slices"
 	"sync"
 )
 
@@ -19,16 +20,24 @@ func createModifyStr(actionEntry actionEntry) (string, error) {
 			if subAction == subActionNone {
 				return "", errors.New(("Invalid Subaction subActionNone for action actionModify"))
 			}
-			for idxInner, attr := range attrList {
+			idxInner := 0
+			for _, attr := range slices.Sorted(maps.Keys(attrList)) {
+				vals := attrList[attr]
 				if idxInner != 0 || idx != 0 {
 					buffer.WriteString("-\n")
 				}
-				parts := strings.Split(attr, ":")
-				buffer.WriteString(subActions[subAction] +
-					": " + parts[0] + "\n")
-				buffer.WriteString(parts[0] + ":" +
-					strings.Join(parts[1:], ":") + "\n")
 
+				idxInner++
+
+				for idxInnerV, val := range vals {
+					if (subActions[subAction] != "add" && subActions[subAction] != "replace") && idxInnerV != 0 {
+						buffer.WriteString("-\n")
+					}
+					if (subActions[subAction] != "add" && subActions[subAction] != "replace") || idxInnerV == 0 {
+						buffer.WriteString(subActions[subAction] + ": " + attr + "\n")
+					}
+					buffer.WriteString(attr + ": " + val + "\n")
+				}
 			}
 		}
 	}
@@ -46,8 +55,11 @@ func writeLdif(queue <-chan actionEntry, writer *bytes.Buffer, delWriter *bytes.
 			writer.WriteString(actionEntry.Dn + "\n") //dn
 			writer.WriteString("changetype: add\n")
 			attrList := actionEntry.SubActionAttrs[0][subActionNone]
-			for _, attr := range attrList {
-				writer.WriteString(attr + "\n")
+			for _, attr := range slices.Sorted(maps.Keys(attrList)) {
+				vals := attrList[attr]
+				for _, val := range vals {
+					writer.WriteString(attr + ": " + val + "\n")
+				}
 			}
 			writer.WriteString("\n")
 		case actionDelete:

t/result_mv.ldif
@@ -1,0 +1,21 @@
+dn: cn=multi1,ou=people,dc=example,dc=com
+changetype: modify
+add: mail
+mail: multi1@example.com
+
+dn: cn=multi3,ou=people,dc=example,dc=com
+changetype: modify
+replace: mail
+mail: multi.kulti2@example.com
+mail: multi2@example.com
+
+dn: cn=multi4,ou=people,dc=example,dc=com
+changetype: modify
+replace: mail
+mail: multi.kulti@example.com
+
+dn: cn=multi5,ou=people,dc=example,dc=com
+changetype: modify
+add: mail
+mail: multi1@example.com
+mail: multi2@example.com

t/source_mv.ldif
@@ -1,0 +1,26 @@
+dn: cn=multi1,ou=people,dc=example,dc=com
+mail: multi1@example.com
+fax: +35434354355
+uid: multi
+
+dn: cn=multi2,ou=people,dc=example,dc=com
+mail: multi2@example.net
+mail: multi.kulti2@example.com
+fax: +35434354355
+uid: multi
+
+dn: cn=multi3,ou=people,dc=example,dc=com
+mail: multi.kulti2@example.com
+mail: multi2@example.com
+fax: +35434354355
+uid: multi
+
+dn: cn=multi4,ou=people,dc=example,dc=com
+uid: multi
+mail: multi.kulti@example.com
+
+dn: cn=multi5,ou=people,dc=example,dc=com
+mail: multi1@example.com
+mail: multi2@example.com
+fax: +35434354355
+uid: multi

t/target_mv.ldif
@@ -1,0 +1,22 @@
+dn: cn=multi1,ou=people,dc=example,dc=com
+fax: +35434354355
+uid: multi
+
+dn: cn=multi2,ou=people,dc=example,dc=com
+mail: multi2@example.net
+mail: multi.kulti2@example.com
+fax: +35434354355
+uid: multi
+
+dn: cn=multi3,ou=people,dc=example,dc=com
+mail: multi2@example.com
+fax: +35434354355
+uid: multi
+
+dn: cn=multi4,ou=people,dc=example,dc=com
+uid: multi
+mail: Multi.Kulti@example.com
+
+dn: cn=multi5,ou=people,dc=example,dc=com
+fax: +35434354355
+uid: multi

cmd/ldifdiff.go
@@ -41,6 +41,7 @@ import (
 type Params struct {
 	Source, Target string
 	IgnoreAttr     []string
+	StrictAttr     []string
 	DnOnly         bool
 }
 
@@ -54,7 +55,7 @@ var usage = versionMsg + "\n" +
   // \\   // \\   // \\   // \\   // \\   // \\   // \\   // \\
 
 Usage:
-  ldifdiff <source> <target> [-i <attributes> ...] [-d]
+  ldifdiff <source> <target> [-i <attributes> ...] [-s <attributes> ...] [-d]
   ldifdiff -h
   ldifdiff -v
 Options:
@@ -62,6 +63,9 @@ Options:
     Only print DNs instead of a full LDIF.
   -i <attributes>, --ignore <attributes>
 	Comma separated attribute list to be ignored.
+	Multiple instances of this switch are allowed.
+  -s <attributes>, --strict <attributes>
+	Comma separated attribute list for which value ordering is to be respected.
 	Multiple instances of this switch are allowed.
   -h, --help
   	Show this screen.
@@ -79,10 +83,10 @@ func main() {
 	switch params.DnOnly {
 	case true:
 		var outputList []string
-		outputList, err = ldifdiff.ListDiffDnFromFiles(params.Source, params.Target, params.IgnoreAttr)
+		outputList, err = ldifdiff.ListDiffDnFromFiles(params.Source, params.Target, params.IgnoreAttr, params.StrictAttr)
 		output = strings.Join(outputList, "\n") + "\n"
 	default:
-		output, err = ldifdiff.DiffFromFiles(params.Source, params.Target, params.IgnoreAttr)
+		output, err = ldifdiff.DiffFromFiles(params.Source, params.Target, params.IgnoreAttr, params.StrictAttr)
 	}
 
 	if err != nil {
@@ -105,6 +109,9 @@ func (params *Params) parse() {
 		fallthrough
 	case args["--ignore"].([]string) != nil:
 		params.IgnoreAttr = args["--ignore"].([]string)
+		fallthrough
+	case args["--strict"].([]string) != nil:
+		params.StrictAttr = args["--strict"].([]string)
 		fallthrough
 	case args["<source>"].(string) != "":
 		params.Source = args["<source>"].(string)

diff.go
@@ -5,6 +5,7 @@ package ldifdiff
 
 import (
 	"bytes"
+	"slices"
 	"sort"
 	"strings"
 	"sync"
@@ -19,7 +20,7 @@ const Author = "Claudio Ramirez <pub.claudio@gmail.com>"
 // Used by the implementation program in the cmd directory.
 const Repo = "https://github.com/tacerus/ldifdiff"
 
-type fn func(string, []string) (entries, error)
+type fn func(string, []string, []string) (entries, error)
 
 var skipDnForDelete map[string]bool
 
@@ -32,8 +33,8 @@ var skipDnForDelete map[string]bool
 // database (the one that created targetStr) in order to make it
 // equal to the "source" database (the one that created sourceStr). In case of
 // failure, an error is provided.
-func Diff(sourceStr, targetStr string, ignoreAttr []string) (string, error) {
-	return genericDiff(sourceStr, targetStr, ignoreAttr, convertLdifStr, nil)
+func Diff(sourceStr, targetStr string, ignoreAttr []string, strictAttr []string) (string, error) {
+	return genericDiff(sourceStr, targetStr, ignoreAttr, strictAttr, convertLdifStr, nil)
 }
 
 // DiffFromFiles compares two LDIF files (sourceFile and targetFile) and
@@ -43,8 +44,8 @@ func Diff(sourceStr, targetStr string, ignoreAttr []string) (string, error) {
 // database (the one that created targetFile) in order to make it equal to the
 // "source" database (the one that created sourceFile). In case of failure, an
 // error is provided.
-func DiffFromFiles(sourceFile, targetFile string, ignoreAttr []string) (string, error) {
-	return genericDiff(sourceFile, targetFile, ignoreAttr, importLdifFile, nil)
+func DiffFromFiles(sourceFile, targetFile string, ignoreAttr []string, strictAttr []string) (string, error) {
+	return genericDiff(sourceFile, targetFile, ignoreAttr, strictAttr, importLdifFile, nil)
 }
 
 // ListDiffDn compares two LDIF strings (sourceStr and targetStr) and outputs
@@ -52,9 +53,9 @@ func DiffFromFiles(sourceFile, targetFile string, ignoreAttr []string) (string, 
 // attributes can be supplied. These attributes will be ignored when comparing
 // the LDIF strings.
 // The output is a string slice. In case of failure, an error is provided.
-func ListDiffDn(sourceStr, targetStr string, ignoreAttr []string) ([]string, error) {
+func ListDiffDn(sourceStr, targetStr string, ignoreAttr []string, strictAttr []string) ([]string, error) {
 	dnList := []string{}
-	_, err := genericDiff(sourceStr, targetStr, ignoreAttr, convertLdifStr, &dnList)
+	_, err := genericDiff(sourceStr, targetStr, ignoreAttr, strictAttr, convertLdifStr, &dnList)
 	return dnList, err
 }
 
@@ -63,13 +64,37 @@ func ListDiffDn(sourceStr, targetStr string, ignoreAttr []string) ([]string, err
 // An array of attributes can be supplied. These attributes will be ignored
 // when comparing the LDIF strings.
 // The output is a string slice. In case of failure, an error is provided.
-func ListDiffDnFromFiles(sourceFile, targetFile string, ignoreAttr []string) ([]string, error) {
+func ListDiffDnFromFiles(sourceFile, targetFile string, ignoreAttr []string, strictAttr []string) ([]string, error) {
 	dnList := []string{}
-	_, err := genericDiff(sourceFile, targetFile, ignoreAttr, importLdifFile, &dnList)
+	_, err := genericDiff(sourceFile, targetFile, ignoreAttr, strictAttr, importLdifFile, &dnList)
 	return dnList, err
 }
 
 /* Package private functions */
+
+func entriesEqual(a, b entry) bool {
+	for attr, vals := range a {
+		bVals, bFound := b[attr]
+		if !bFound {
+			return false
+		}
+		if !slices.Equal(vals, bVals) {
+			return false
+		}
+	}
+
+	for attr, vals := range b {
+		aVals, aFound := a[attr]
+		if !aFound {
+			return false
+		}
+		if !slices.Equal(vals, aVals) {
+			return false
+		}
+	}
+
+	return true
+}
 
 func arraysEqual(a, b []string) bool {
 	if a == nil && b == nil {
@@ -99,7 +124,7 @@ func arraysEqual(a, b []string) bool {
 // - extra attribute on source: actionAdd
 // - extra attribute on target: delete
 
-func compare(source, target *entries, dnList *[]string) (string, error) {
+func compare(source, target *entries, dnList *[]string, strictAttr []string) (string, error) {
 	var buffer bytes.Buffer
 	var delBuffer bytes.Buffer
 	var err error
@@ -122,7 +147,7 @@ func compare(source, target *entries, dnList *[]string) (string, error) {
 	sendForDeletion(&orderedTargetLongToShort, source, target, queue, dnList)
 
 	// Dn on source and target
-	sendForModification(&orderedSourceShortToLong, source, target, queue, dnList)
+	sendForModification(&orderedSourceShortToLong, source, target, queue, dnList, strictAttr)
 
 	// Done sending work
 	close(queue)
@@ -147,20 +172,20 @@ func compare(source, target *entries, dnList *[]string) (string, error) {
 //	return false
 //}
 
-func genericDiff(sourceParam, targetParam string, ignoreAttr []string, fn fn, dnList *[]string) (string, error) {
+func genericDiff(sourceParam, targetParam string, ignoreAttr, strictAttr []string, fn fn, dnList *[]string) (string, error) {
 	// Read the files in memory as a Map with sorted attributes
 	var source, target entries
 	var sourceErr, targetErr error
 	var wg sync.WaitGroup
 	wg.Add(2)
 	go func(entries *entries, wg *sync.WaitGroup, err *error) {
-		result, e := fn(sourceParam, ignoreAttr)
+		result, e := fn(sourceParam, ignoreAttr, strictAttr)
 		*entries = result
 		*err = e
 		wg.Done()
 	}(&source, &wg, &sourceErr)
 	go func(entries *entries, wg *sync.WaitGroup, err *error) {
-		result, e := fn(targetParam, ignoreAttr)
+		result, e := fn(targetParam, ignoreAttr, strictAttr)
 		*entries = result
 		*err = e
 		wg.Done()
@@ -175,7 +200,7 @@ func genericDiff(sourceParam, targetParam string, ignoreAttr []string, fn fn, dn
 	}
 
 	// Compare the files
-	return compare(&source, &target, dnList)
+	return compare(&source, &target, dnList, strictAttr)
 }
 
 func sendForAddition(
@@ -184,16 +209,10 @@ func sendForAddition(
 	queue chan<- actionEntry,
 	dnList *[]string) {
 	for _, dn := range *orderedSourceShortToLong {
-		// Ignore equal entries
-		if arraysEqual((*source)[dn], (*target)[dn]) {
-			delete(*source, dn)
-			delete(*target, dn)
-			continue
-		}
 		// Mark entries for addition if only on source
 		if _, ok := (*target)[dn]; !ok {
 			if dnList == nil {
-				subActionAttr := make(map[subAction][]string)
+				subActionAttr := make(map[subAction]entry)
 				subActionAttr[subActionNone] = (*source)[dn]
 				actionEntry :=
 					actionEntry{
@@ -211,6 +230,13 @@ func sendForAddition(
 		// Implict else:
 		// It exists on target and it's not equal, so it's a modifyStr
 		skipDnForDelete[dn] = true
+
+		if entriesEqual((*source)[dn], (*target)[dn]) {
+			delete(*source, dn)
+			delete(*target, dn)
+			continue
+		}
+
 	}
 }
 
@@ -226,7 +252,7 @@ func sendForDeletion(
 		if _, ok := (*target)[dn]; ok { // It has not been deleted above
 			if _, ok := (*source)[dn]; !ok { // does not exists on source
 				if dnList == nil {
-					subActionAttr := make(map[subAction][]string)
+					subActionAttr := make(map[subAction]entry)
 					subActionAttr[subActionNone] = nil
 					actionEntry :=
 						actionEntry{
@@ -254,49 +280,62 @@ func sendForModification(
 	orderedSourceShortToLong *[]string, source,
 	target *entries,
 	queue chan<- actionEntry,
-	dnList *[]string) {
+	dnList *[]string,
+	strictAttr []string,
+) {
 	for _, dn := range *orderedSourceShortToLong {
 		// DN is present on source and target:
 		// sendForAdd/Remove clean up source and target
 		_, okSource := (*source)[dn]
 		_, okTarget := (*target)[dn]
+
 		if okSource && okTarget { // it hasn't been deleted
 			if dnList == nil {
 
 				// Store the attributes to be added, deleted or replaced
-				attrToModifyAdd := []string{}
-				attrToModifyDelete := []string{}
-				attrToModifyReplace := []string{}
+				attrToModifyAdd := entry{}
+				attrToModifyDelete := entry{}
+				attrToModifyReplace := entry{}
+
 				// Put the attributes in a map for easy lookup
-				sourceAttr := make(map[string]bool)
-				targetAttr := make(map[string]bool)
-				for _, attr := range (*source)[dn] {
-					sourceAttr[attr] = true
+				sourceMap := make(map[string][]string)
+				for attr, vals := range (*source)[dn] {
+					sourceMap[attr] = vals
 				}
-				for _, attr := range (*target)[dn] {
-					targetAttr[attr] = true
+				targetMap := make(map[string][]string)
+				for attr, vals := range (*target)[dn] {
+					targetMap[attr] = vals
 				}
 
-				// Compare attribute values starting from the source
-				for _, attr := range (*source)[dn] { // Keep the order of the attributes
-					// Attribute is not equal on both sides
-					if _, ok := targetAttr[attr]; !ok {
-						// Is the attribute name (not value) unique?
-						switch uniqueAttrName(attr, sourceAttr, targetAttr) {
-						case true: // This is a actionModify-Replace operation
-							attrToModifyReplace = append(attrToModifyReplace, attr)
-						case false: // This is just a actionModify actionAdd (only on source).
-							attrToModifyAdd = append(attrToModifyAdd, attr)
+				for attr, sourceVals := range (*source)[dn] {
+					targetVals, ok := targetMap[attr]
+					if ok && !slices.Equal(sourceVals, targetVals) || !ok {
+						strict := slices.Contains(strictAttr, attr)
+						lS := len(sourceVals)
+						lT := len(targetVals)
+						if (strict && (lS > 0 && lT > 0)) || (!strict && (lS == 1 && lT == 1)) {
+							attrToModifyReplace[attr] = sourceVals
+						} else {
+							for _, val := range sourceVals {
+								if !slices.Contains(targetVals, val) {
+									attrToModifyAdd[attr] = append(attrToModifyAdd[attr], val)
+								}
+							}
 						}
 					}
 				}
 
 				// Compare attribute values starting from the target.
-				for _, attr := range (*target)[dn] { // Keep the order of the attributes
+				for attr, targetVals := range (*target)[dn] {
+					sourceVals, ok := (*source)[dn][attr]
 					// Looking for unique attributes
-					if !uniqueAttrName(attr, sourceAttr, targetAttr) {
-						if _, ok := sourceAttr[attr]; !ok {
-							attrToModifyDelete = append(attrToModifyDelete, attr)
+					if !ok && !(len(sourceVals) == 1 && len(targetVals) == 1) {
+						attrToModifyDelete[attr] = targetVals
+					} else if _, ok := attrToModifyReplace[attr]; !ok {
+						for _, val := range targetVals {
+							if !slices.Contains(sourceVals, val) {
+								attrToModifyDelete[attr] = append(attrToModifyDelete[attr], val)
+							}
 						}
 					}
 				}
@@ -374,29 +413,4 @@ func sortDnByDepth(entries *entries, longToShort bool) []string {
 	}
 
 	return sorted
-}
-
-func uniqueAttrName(attr string, sourceAttr, targetAttr map[string]bool) bool {
-
-	// Get the attribute name
-	parts := strings.Split(attr, ":")
-	attrName := parts[0]
-	//var base64 bool
-	//if parts[1] == "" {
-	//	base64 = true
-	//}
-	sourceCounter := 0
-	for attr := range sourceAttr {
-		if strings.HasPrefix(attr, attrName+":") {
-			sourceCounter++
-		}
-	}
-	targetCounter := 0
-	for attr := range targetAttr {
-		if strings.HasPrefix(attr, attrName+":") {
-			targetCounter++
-		}
-	}
-
-	return sourceCounter == 1 && targetCounter == 1
 }

diff_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestDiff(t *testing.T) {
-	ldif, err := Diff(testSourceStr, testTargetStr, nil)
+	ldif, err := Diff(testSourceStr, testTargetStr, nil, nil)
 	if ldif != testResultStr {
 		t.Error("Expected:\n[" + testResultStr + "]\nGot:\n[" + ldif + "]\n")
 	}
@@ -18,7 +18,7 @@ func TestDiff(t *testing.T) {
 		t.Error("Expected changes, got an empty modifyStr")
 	}
 
-	ldif, err = Diff(testSourceStr, testTargetStr, testIgnoreAttr)
+	ldif, err = Diff(testSourceStr, testTargetStr, testIgnoreAttr, nil)
 	if err != nil {
 		t.Error("Expected values, got error: ", err)
 	}
@@ -32,8 +32,21 @@ func TestDiff(t *testing.T) {
 	}
 }
 
+func TestDiffMv(t *testing.T) {
+	ldif, err := Diff(testSourceMvStr, testTargetMvStr, nil, testStrictAttr)
+	if ldif != testResultMvStr {
+		t.Error("Expected: " + testResultMvStr + " Got: " + ldif)
+	}
+	if err != nil {
+		t.Error("Expected values, got error: ", err)
+	}
+	if ldif == "" {
+		t.Error("Expected changes, got an empty modifyStr")
+	}
+}
+
 func TestDiffFromFiles(t *testing.T) {
-	ldif, err := DiffFromFiles(testSourceLdifFile, testTargetLdifFile, nil)
+	ldif, err := DiffFromFiles(testSourceLdifFile, testTargetLdifFile, nil, nil)
 	if ldif != testResultStr {
 		t.Error("Expected:\n[" + testResultStr + "]\nGot:\n[" + ldif + "]\n")
 	}
@@ -44,7 +57,7 @@ func TestDiffFromFiles(t *testing.T) {
 		t.Error("Expected changes, got an empty modifyStr")
 	}
 
-	ldif, err = DiffFromFiles(testSourceLdifFile, testTargetLdifFile, testIgnoreAttr)
+	ldif, err = DiffFromFiles(testSourceLdifFile, testTargetLdifFile, testIgnoreAttr, nil)
 	if err != nil {
 		t.Error("Expected values, got error: ", err)
 	}
@@ -59,7 +72,7 @@ func TestDiffFromFiles(t *testing.T) {
 }
 
 func TestListDiffDn(t *testing.T) {
-	dns, err := ListDiffDn(testSourceStr, testTargetStr, nil)
+	dns, err := ListDiffDn(testSourceStr, testTargetStr, nil, nil)
 	joinedLines := strings.Join(dns, "\n") + "\n"
 	if joinedLines != testResultDnStr {
 		t.Error("Expected:\n[" + testResultDnStr + "]\nGot:\n[" + joinedLines + "]\n")
@@ -68,7 +81,7 @@ func TestListDiffDn(t *testing.T) {
 		t.Error("Expected values, got error: ", err)
 	}
 
-	dns, err = ListDiffDn(testSourceStr, testTargetStr, testIgnoreAttrDn)
+	dns, err = ListDiffDn(testSourceStr, testTargetStr, testIgnoreAttrDn, nil)
 	joinedLines = strings.Join(dns, "\n") + "\n"
 	if joinedLines != testResultDnIgnoreAttrStr {
 		t.Error("Expected:\n[" + testResultDnIgnoreAttrStr + "]\nGot:\n[" + joinedLines + "]\n")
@@ -79,7 +92,7 @@ func TestListDiffDn(t *testing.T) {
 }
 
 func TestListDiffDnFromFiles(t *testing.T) {
-	dns, err := ListDiffDnFromFiles(testSourceLdifFile, testTargetLdifFile, nil)
+	dns, err := ListDiffDnFromFiles(testSourceLdifFile, testTargetLdifFile, nil, nil)
 	joinedLines := strings.Join(dns, "\n") + "\n"
 	if joinedLines != testResultDnStr {
 		t.Error("Expected:\n[" + testResultDnStr + "]\nGot:\n[" + joinedLines + "]\n")
@@ -93,7 +106,7 @@ func TestDiffFromFilesBig(t *testing.T) {
 	if os.Getenv(testBigFilesEnv) != testBigFilesEnvValue {
 		t.Skip("Skipping big files test")
 	}
-	ldif, err := DiffFromFiles(testSourceLdifFileBig, testTargetLdifFileBig, nil)
+	ldif, err := DiffFromFiles(testSourceLdifFileBig, testTargetLdifFileBig, nil, nil)
 	if ldif != testResultStr {
 		t.Error("Expected:\n[" + testResultStrBig + "]\nGot:\n[" + ldif + "]\n")
 	}
@@ -109,7 +122,7 @@ func TestDiffBig(t *testing.T) {
 	if os.Getenv(testBigFilesEnv) != testBigFilesEnvValue {
 		t.Skip("Skipping big files test")
 	}
-	ldif, err := Diff(testSourceStrBig, testTargetStrBig, nil)
+	ldif, err := Diff(testSourceStrBig, testTargetStrBig, nil, nil)
 	if ldif != testResultStr {
 		t.Error("Expected:\n" + testResultStrBig + "Got:\n" + ldif)
 	}
@@ -120,7 +133,7 @@ func TestDiffBig(t *testing.T) {
 		t.Error("Expected changes, got an empty modifyStr")
 	}
 
-	ldif, err = Diff(testSourceStrBig, testTargetStrBig, testIgnoreAttr)
+	ldif, err = Diff(testSourceStrBig, testTargetStrBig, testIgnoreAttr, nil)
 	if err != nil {
 		t.Error("Expected values, got error: ", err)
 	}
@@ -135,9 +148,9 @@ func TestDiffBig(t *testing.T) {
 }
 
 func TestCompare(t *testing.T) {
-	source, _ := importLdifFile(testSourceLdifFile, nil)
-	target, _ := importLdifFile(testTargetLdifFile, nil)
-	ldif, err := compare(&source, &target, nil)
+	source, _ := importLdifFile(testSourceLdifFile, nil, nil)
+	target, _ := importLdifFile(testTargetLdifFile, nil, nil)
+	ldif, err := compare(&source, &target, nil, nil)
 	if ldif != testResultStr {
 		t.Error("Expected:\n[" + testResultStr + "]\nGot:\n[" + ldif + "]\n")
 	}

commit 48922da1f55d19dfbd289a8110568e54f8b6f088

Handle Base64 again
One of the two colons in such attributes was added to the attribute
name, causing the matching logic to not work and the resulting LDIF to
contain attribute references with bogus trailing colons (for example:
"delete: cn:").

The solution here is really ugly, as we first need to extract the
attribute name regardless of whether the value is Base64 in order for it
to match during comparison, but later need to again check whether the value
is Base64 in order to write a respective double colon separator. Cleaner
would probably be to track the Base64 status in a struct for each entry,
but then the whole parsing might better just be rewritten to go-ldap/ldif...

Fixes: a5dffb69283a ("Support attributes with strictly ordered values")
Signed-off-by: Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>

input.go
@@ -203,7 +203,7 @@ func parse(entries entries, strictAttr []string, queue <-chan []string, wg *sync
 
 		for _, line := range rest {
 			parts := strings.SplitN(line, ": ", 2)
-			attr := parts[0]
+			attr := strings.TrimSuffix(parts[0], ":")
 			val := parts[1]
 			newEntry[attr] = append(newEntry[attr], val)
 		}

ldifConvertor.go
@@ -2,10 +2,12 @@ package ldifdiff
 
 import (
 	"bytes"
+	"encoding/base64"
 	"errors"
 	"fmt"
 	"maps"
 	"slices"
+	"strings"
 	"sync"
 )
 
@@ -36,7 +38,12 @@ func createModifyStr(actionEntry actionEntry) (string, error) {
 					if (subActions[subAction] != "add" && subActions[subAction] != "replace") || idxInnerV == 0 {
 						buffer.WriteString(subActions[subAction] + ": " + attr + "\n")
 					}
-					buffer.WriteString(attr + ": " + val + "\n")
+					sep := ":"
+					_, err := base64.StdEncoding.DecodeString(val)
+					if err == nil && !strings.HasPrefix(val, "+") {
+						sep = "::"
+					}
+					buffer.WriteString(attr + sep + " " + val + "\n")
 				}
 			}
 		}

t/result_mv.ldif
@@ -19,3 +19,13 @@ changetype: modify
 add: mail
 mail: multi1@example.com
 mail: multi2@example.com
+
+dn: cn=multi6,ou=people,dc=example,dc=com
+changetype: modify
+replace: cn
+cn:: TcOkeCBNw7xzdGVybcOkbm4=
+
+dn: cn=multi7,ou=people,dc=example,dc=com
+changetype: modify
+replace: cn
+cn: Max Mustermann

t/source_mv.ldif
@@ -24,3 +24,9 @@ mail: multi1@example.com
 mail: multi2@example.com
 fax: +35434354355
 uid: multi
+
+dn: cn=multi6,ou=people,dc=example,dc=com
+cn:: TcOkeCBNw7xzdGVybcOkbm4=
+
+dn: cn=multi7,ou=people,dc=example,dc=com
+cn: Max Mustermann

t/target_mv.ldif
@@ -20,3 +20,9 @@ mail: Multi.Kulti@example.com
 dn: cn=multi5,ou=people,dc=example,dc=com
 fax: +35434354355
 uid: multi
+
+dn: cn=multi6,ou=people,dc=example,dc=com
+cn: Max Mustermann
+
+dn: cn=multi7,ou=people,dc=example,dc=com
+cn:: TcOkeCBNw7xzdGVybcOkbm4=